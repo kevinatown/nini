5630d3ddb79a3ccb8709c9693c6243e0186b7501
Cristina Townsend <Crisitna@Cristinas-MacBook-Pro-2.local>
2018-07-05 11:18:29 -0400

    My first commit

---
+++ week1/notes/notes.bash
@@ -0,0 +1,8 @@
+# NOTES for you
+
+#command options args
+mv -v argumtnent1
+
+mv route/to/target.file route/to/destination.file
+mv route/from/target.file route/to/target.file
+mv route/from/target.file route/to/destination.file
