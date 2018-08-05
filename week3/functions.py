290d7a1bb6cd11caa4fe310afcfd4215b06debfc
kevinatown <kevinatown@gmail.com>
2018-08-04 20:30:49 -0400

    updated functions.py

--- week3/functions.py
+++ week3/functions.py
@@ -10,10 +10,79 @@ def onePlusOne():
 	# do work
 	return 1 + 1
 
+def stringToInts(array):
+  """
+  Takes an array, convets all potential ints
+  
+  Arguments: 
+  array -- list of items
+
+  Returns:
+  an array with all items as ints, and none ints removes
+  """
+  returnArray = []
+  for item in array:
+    # this is a simple for loop, python makes it easy to iterate over each item in a list
+    try:
+      # this is a try catch statement, dont worry about it now
+      num = int(item)
+      returnArray.append(num)
+    except ValueError:
+      print('%s is not an int' % item)
+  return returnArray
+
+def removeDuplicates(array):
+  """
+  Removes duplicates from a list
+
+  Arguments: 
+  array -- list of items
+
+  Returns:
+  an array with duplicates removed
+  """
+  pass
+
+def sumArray(array):
+  """
+    Takes an array of numbers and sums it.
+    
+    Arguments:
+    array -- list of ints
+    
+    Returns:
+    sum -- int
+  """
+  pass
+
+
+
 def main():
-	# main function put your other functions in here so that you can run this as a script
-	returnVal = onePlusOne()
-	print returnVal
+  """main function put your other functions in here so that you can run this as a script"""
+  
+  returnVal = onePlusOne()
+  print returnVal
+
+  l0 = ['1', 2, 'three', '6']
+  l0n = stringToInts(l0)
+
+  # expected [1, 2, 6] and 9
+  print(l0n, sum(l0n))
+
+  # create other test cases
+  l1 = [1, 2, 3, 4, 5, 6]
+  r1 = sumArray(l1)
+  # I expect this to be 21
+  print(r1)
+
+  r2 = sum(l1)
+  validate1 = r2 == r1
+
+  # expected 21, true
+  print(r2, validate1)
+
+  # removing duplicates, write test cases here
+
 
 # dont worry about this yet
 if __name__ == '__main__':
